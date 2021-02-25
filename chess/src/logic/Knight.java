95607dbe1df0816805681cafb0bbff97c79f7c68
Areet Sheth <areetsheth@gmail.com>
2021-02-25 12:50:31 -0500

    Update Knight.java

--- chess/src/logic/Knight.java
+++ chess/src/logic/Knight.java
@@ -2,23 +2,20 @@ package logic;
 
 import javax.swing.JLabel;
 
-public class Knight extends Piece{
-	
+public class Knight extends Piece {
+
 	public String Icon;
 
 	public String Color;
 
 	public boolean isColor;
 
-	public Knight(String color, boolean isColor) {		
+	public Knight(String color, boolean isColor) {
 		super("Knight", color, isColor);
 	}
-
 
 	public String toString() {
 		return Icon;
 	}
 
 }
-	
-	
